dacd90165f7b3238cdddb6b41abf048da8f1093f
Antboi123 <sam3erj20@gmail.com>
2025-09-15 08:49:01 +0200

    Add Debug the Bug coding mini-game

---
+++ playground/coding-games/debug-the-bug.js
@@ -0,0 +1,14 @@
+// Simple "Debug the Bug" coding mini-game for kids
+// Find and fix the bug in this code!
+
+function countToFive() {
+  let result = "";
+  for (let i = 1; i <= 5; i++) {
+    // Oops! There's a bug here, can you fix it?
+    result += i + ",";
+  }
+  return result;
+}
+
+// To play: Look for the bug and try to fix it!
+// Hint: What happens at the end of the string?
