5ad52d6316e6beab7b6764fbacaabbd793be806a
Antboi123 <sam3erj20@gmail.com>
2025-09-15 08:49:24 +0200

    Add code art generator for ASCII smiley

---
+++ playground/code-art/generate-art.js
@@ -0,0 +1,14 @@
+// Generates a simple smiley face in ASCII art
+
+function generateSmiley() {
+  return `
+      _____
+     /     \
+    | () () |
+     \  ^  /
+      |||||
+      |||||
+  `;
+}
+
+console.log(generateSmiley());
