acf979bddea0f4242867d2085941b8df7976d716
Antboi123 <sam3erj20@gmail.com>
2025-09-15 09:33:47 +0200

    Add game template file

---
+++ playground/coding-games/game-template.js
@@ -0,0 +1,13 @@
+// Game Template: Copy this file to start your own coding game!
+
+/*
+  Game name: [Your Game Name]
+  Goal: [Describe the goal]
+  How to play: [Instructions]
+*/
+
+function startGame() {
+  // Write your game logic here!
+}
+
+startGame();
